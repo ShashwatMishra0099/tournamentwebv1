1969cea9f4a9bf8cf01cd8593ea0474d63d2935b
Shashwat <163753719+ShashwatMishra0099@users.noreply.github.com>
2025-10-22 17:28:13 +0530

    Create bottom.php

---
+++ Adept-Play/common/bottom.php
@@ -0,0 +1,27 @@
+</main>
+
+<footer class="fixed bottom-3 left-0 right-0 z-50">
+  <div class="max-w-md mx-auto px-4">
+    <div class="bg-gray-900/80 backdrop-blur rounded-2xl p-3 flex justify-between items-center shadow-lg border border-gray-800">
+      <a href="index.php" class="flex flex-col items-center text-xs">
+        <i class="fa-solid fa-house text-lg"></i>
+        <span>Home</span>
+      </a>
+      <a href="my_tournaments.php" class="flex flex-col items-center text-xs">
+        <i class="fa-solid fa-list-check text-lg"></i>
+        <span>My</span>
+      </a>
+      <a href="wallet.php" class="flex flex-col items-center text-xs">
+        <i class="fa-solid fa-wallet text-lg"></i>
+        <span>Wallet</span>
+      </a>
+      <a href="profile.php" class="flex flex-col items-center text-xs">
+        <i class="fa-solid fa-user text-lg"></i>
+        <span>Profile</span>
+      </a>
+    </div>
+  </div>
+</footer>
+
+</body>
+</html>
